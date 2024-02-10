8dde15f0f0f87ef18e6b7c9be37b3f5ea799aa05
DeoCtrsMtz <154368280+DeoCtrsMtz@users.noreply.github.com>
2024-02-09 23:54:32 -0600

    Add files via upload

---
+++ RegistroPagoCuotas.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const RegistroPagoCuotas = () => {
+  return (
+    <div>
+        <h2>Registro Pago Cuotas</h2>
+        <ul>
+            <li>Diseño de la Interfaz de Registro de Pagos</li>
+            <li>Implementación del Registro de Pagos en la Base de Datos</li>
+            <li>Integración del Registro de Pagos con el Sistema de Notificaciones</li>
+            <li>Pruebas y Correcciones del Módulo de Pagos</li>
+        </ul>
+        </div>
+  )
+}
---
+++ SistemaPagoOnline.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const SistemaPagoOnline = () => {
+  return (
+    <div>
+        <h2>Sistema Pago Online</h2>
+        <ul>
+            <li>Diseño de la Interfaz de Pago Online</li>
+            <li>Implementación del Sistema de Pago Online</li>
+            <li>Integración del Pago Online con el Registro de Pagos</li>
+            <li>Pruebas y Correcciones del Módulo de Pago Online</li>
+        </ul>
+        </div>
+  )
+}
